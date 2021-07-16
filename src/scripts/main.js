43fcfd56db73dae362f4c5784de496457e98df5b
Rahul Shaw <shawrahul2006@gmail.com>
2021-07-16 16:03:09 +0530

    Navbar Added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 import "../scss/styles.scss";
+
+// Importing Variables
+const mobileNav = document.getElementById("mobileNav");
+const hamburgerOpen = document.getElementById("hamburgerOpen");
+const hamburgerClose = document.getElementById("hamburgerClose");
+
+hamburgerOpen.addEventListener("click", () => {
+  mobileNav.classList.remove("hide");
+  hamburgerClose.classList.remove("hide");
+  hamburgerOpen.classList.add("hide");
+});
+
+hamburgerClose.addEventListener("click", () => {
+  mobileNav.classList.add("hide");
+  hamburgerClose.classList.add("hide");
+  hamburgerOpen.classList.remove("hide");
+});
